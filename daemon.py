80e3fe7e56a46c6957287accb5eb803dceae915c
Theo Nakfoor <theonakfoor@gmail.com>
2025-07-25 20:07:38 -0700

    debug: Debugging build.

--- daemon.py
+++ daemon.py
@@ -254,7 +254,7 @@ if __name__ == "__main__":
             generate_dockerfile(container_id)
 
             build_process = subprocess.Popen(
-                'script -q -c "docker build -t job-' + str(job_id) + ' /tmp/cloudcontain-jobs/' + str(container_id) + ' 2> >(while read line; do echo \\"[STDERR] $line\\"; done) | while read line; do echo \\"[STDOUT] $line\\"; done; exit ${PIPESTATUS[0]}"',
+                'bash -c "docker build -t job-' + str(job_id) + ' /tmp/cloudcontain-jobs/' + str(container_id) + ' 2> >(while read line; do echo \\"[STDERR] $line\\"; done) | while read line; do echo \\"[STDOUT] $line\\"; done; exit ${PIPESTATUS[0]}"',
                 shell=True,
                 stdout=subprocess.PIPE,
                 universal_newlines=True,
